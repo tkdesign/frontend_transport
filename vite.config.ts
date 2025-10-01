44350d8b12a93f466b3c8aa7f68bd67abf310648
Petr <kpa80@yandex.ru>
2025-10-01 18:09:52 +0200

    enhance Vite configuration to load environment variables and set server options

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,26 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
-  },
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), '')
+    return {
+        plugins: [
+            vue(),
+            vueDevTools(),
+        ],
+        resolve: {
+            alias: {
+                '@': fileURLToPath(new URL('./src', import.meta.url))
+            },
+        },
+        server: {
+            allowedHosts: [env.VITE_API_BASE_URL.replace(/^https?:\/\//, ''), 'localhost'],
+            host: '0.0.0.0',
+            port: 3000,
+        }
+    }
 })
