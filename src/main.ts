48648804231a86ca5d7519fe5206f8cdeab092a3
Petr <kpa80@yandex.ru>
2025-10-01 18:31:55 +0200

    add Vuetify integration

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,15 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+
+const vuetify = createVuetify()
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(vuetify)
 
 app.mount('#app')
